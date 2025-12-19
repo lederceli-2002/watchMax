44f737839e408635164b0bbec0026e7f14d88832
leder <lederceli2002@gmail.com>
2025-12-19 16:25:00 -0500

    Agrego listado de categorías y posters

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 //carrusel
-var imgs=new Array(4);//los arrays se enumeran desde el 0,1,2,3 = 4elemets
-imgs[0]="/imagenes-index/banner1.jpg";
-imgs[1]="../imagenes-index/banner.jpg";
-imgs[2]="../imagenes/baner/baner3.jpg";
-imgs[3]="../imagenes/baner/baner4.jpg";
+let imgs=[
+  "imagenes-index/banner.jpg",
+  "imagenes-index/banner1.jpg",
+  "imagenes-index/banner2.jpg",
+  "imagenes-index/banner3.jpg"
+]
 
-var c=0; //variable de control para el indice
+let c=0;
+
 function carrusel()
 {
-    document.getElementById("ban").src=imgs[c];//imgs es el array y "c"el indice 
-    c++;
-    if(c>=imgs.length) c=0;//o igual y mayor que la cantidad de elemetos del array
-    setTimeout("carrusel()",2000);
+ document.getElementById("ban").src=imgs[c];
+ c++;
+ if (c >= imgs.length){
+    c=0;
+ }  
 }
-window.onload=carrusel;
+setInterval(carrusel,2000);
